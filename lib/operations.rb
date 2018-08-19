6985f22eefdf4e8fde92edcbd07fa9974a8e6ee1
Jackie Ha <jha2@wellesley.edu>
2018-08-19 01:13:42 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,11 @@
 def unsafe?(speed)
-
-end
-
-
+  if speed < 40 or speed > 60
+    true
+  else speed > 40 and speed < 60
+    false 
+  end 
+end 
 
 def not_safe?(speed)
-	
+	speed < 40 or speed > 60 ? true : false
 end
-	
-
-
