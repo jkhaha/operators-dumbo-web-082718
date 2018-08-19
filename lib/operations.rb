66511dd19127582b9872b1ebbf5f1f02604971e3
Jackie Ha <jha2@wellesley.edu>
2018-08-19 01:00:52 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,16 @@
+require 'pry'
 def unsafe?(speed)
-
-end
-
-
+  if speed < 40 or speed > 60
+    true
+  else speed (40..60)
+    false 
+  end 
+end 
 
 def not_safe?(speed)
-	
+	speed < 40 or > 60 ? : false
 end
 	
-
+binding.pry
+puts goodbye
 
